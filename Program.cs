a9616cb75de471e8ebdbdac3facf0daf16ce06d6
Vitor Appolinario <vitor@appolinario.com>
2023-01-27 11:26:58 -0300

    Algumas refatorações para legibilidade

--- Program.cs
+++ Program.cs
@@ -1,36 +1,24 @@
 ﻿using Newtonsoft.Json;
 using Microsoft.Extensions.Configuration;
 
-const string GPT_MODEL = "text-davinci-001";
-
 if (args.Length > 0)
 {
-    var content = new StringContent(
-            JsonConvert.SerializeObject(new
-            {
-                temperature = 1,
-                max_tokens = 100,
-                model = GPT_MODEL,
-                prompt = args[0]
-            }),
-    System.Text.Encoding.UTF8, "application/json");
-
-    var result = GetCompletion(content).Result;
-
     try
     {
+        var payload = CreatePayload(args[0]);
+        var result = GetCompletion(payload).Result;
+
         var dyData = JsonConvert.DeserializeObject<dynamic>(result);
         string text = dyData!.choices[0].text;
         var guess = GuessCommand(text);
-        Console.ForegroundColor = ConsoleColor.Green;
-        System.Console.WriteLine($"O Comando é {guess}");
-        Console.ResetColor();
 
+        Console.ForegroundColor = ConsoleColor.Green;
+        System.Console.WriteLine($"{guess}");
+        Console.ResetColor();
     }
     catch (System.Exception ex)
     {
         System.Console.WriteLine(ex.Message);
-        return;
     }
 }
 else
@@ -38,18 +26,34 @@ else
     Console.WriteLine("usage: ask 'pergunta?'");
 }
 
+static StringContent CreatePayload(string prompt)
+{
+    const string GPT_MODEL = "text-davinci-001";
+    var content = new StringContent(
+            JsonConvert.SerializeObject(new
+            {
+                temperature = 1,
+                max_tokens = 100,
+                model = GPT_MODEL,
+                prompt = prompt
+            }),
+    System.Text.Encoding.UTF8, "application/json");
+    return content;
+}
+
 static async Task<string> GetCompletion(StringContent content)
 {
     var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
-
     var secretProvider = config.Providers.First();
     secretProvider.TryGet("Openai:ServiceApiKey", out var apiKey);
 
-    var client = new HttpClient();
-    client.DefaultRequestHeaders.Add("authorization", $"Bearer {apiKey}");
-    var response = await client.PostAsync("https://api.openai.com/v1/completions", content);
-    var responseString = await response.Content.ReadAsStringAsync();
-    return responseString;
+    using (var client = new HttpClient())
+    {
+        client.DefaultRequestHeaders.Add("authorization", $"Bearer {apiKey}");
+        var response = await client.PostAsync("https://api.openai.com/v1/completions", content);
+        var responseString = await response.Content.ReadAsStringAsync();
+        return responseString;
+    }
 }
 
 static string GuessCommand(string raw)
